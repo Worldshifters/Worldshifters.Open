5ab4281a9a3a336a173e50a912602fd35a49f97a
Masuzu <anveena.menethil@gmail.com>
2019-08-08 00:51:48 +0100

    Add commonly used healing abilities/animations

---
+++ Worldshifters.Abstractions/Hero/Abilities/HealingAbilities.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="HealingAbilities.cs" company="Worldshifters">
+// Copyright (c) Worldshifters. All rights reserved.
+// </copyright>
+
+namespace Worldshifters.Data.Hero.Abilities
+{
+    using System;
+    using Worldshifters.Data.Hero;
+
+    public static class HealingAbilities
+    {
+        public static Ability ClearAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Ability ReviveAnimation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
