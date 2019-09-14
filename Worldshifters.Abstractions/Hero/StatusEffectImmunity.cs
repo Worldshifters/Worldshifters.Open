38b155b750b254768e7cc4062d3eca4f703821c9
Masuzu <anveena.menethil@gmail.com>
2019-09-14 21:22:04 +0100

    Add missing StatusEffectImmunity metadata

---
+++ Worldshifters.Abstractions/Hero/StatusEffectImmunity.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="StatusEffectImmunity.cs" company="Worldshifters">
+// Copyright (c) Worldshifters. All rights reserved.
+// </copyright>
+
+namespace Worldshifters.Data.Hero
+{
+    using System;
+    using Google.Protobuf;
+    using Worldshifters.Data.Raid;
+
+    public class StatusEffectImmunity
+    {
+        /// <summary>
+        /// The <see cref="StatusEffectSnapshot.Id"/> to which the character will be immune to.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The <see cref="Modifier"/> to which the character will be immune to.
+        /// </summary>
+        public Modifier Modifier { get; set; }
+
+        public ByteString ToByteString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
